4be807de88044fbba2d44c9cd7e01ab1f980225e
Bautista Soto <bautistasoto918@gmail.com>
2026-05-15 20:46:41 -0300

    Create config for supabase

---
+++ src/config/Supabase.js
@@ -0,0 +1,8 @@
+import {createClient} from '@supabase/supabase-js';
+import dotenvt from 'dotenv';
+
+dotenvt.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);    
