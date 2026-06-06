16d402ae5cc4d72725da40acd8573802236ff3b3
Bautista <bautistasoto918@gmail.com>
2026-06-06 18:14:48 -0300

    feat: simplify dotenv configuration and restore vite-env.d.ts file

--- servidor/src/config/Supabase.js
+++ servidor/src/config/Supabase.js
@@ -1,8 +1,7 @@
 import {createClient} from '@supabase/supabase-js';
 import dotenv from 'dotenv';
-import path from 'path';
 
-dotenv.config({ path: path.join(process.cwd(), '.env') });
+dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
